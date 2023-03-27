a122522c825cfeb6909a1571190b5b595d70ed48
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-03-27 18:01:36 +0300

    wip

--- jenkins-universal-ansible-wrapper-w-lib.groovy
+++ jenkins-universal-ansible-wrapper-w-lib.groovy
@@ -1,13 +1,40 @@
 #!/usr/bin/env groovy
 
-@Library('jenkins-shared-library-alx@development') _
+
+import groovy.json.JsonBuilder
+
+
+@Library('jenkins-shared-library-alx@devel') _
+
+
+// Repo URL and a branch of 'ansible-wrapper-settings' to load current pipeline settings from.
+def pipelineSettingsGitUrl = 'https://github.com/alexanderbazhenoff/ansible-collection-linux.git' as String
+def pipelineSettingsGitBranch = 'main' as String
+// Prefix for pipeline settings path inside the 'ansible-wrapper-settings' project, that will be added automatically
+// on yaml load.
+def pipelineSettingsPathPrefix = 'settings/' as String
+// Postfix for pipeline settings path inside the 'ansible-wrapper-settings' project, that will be added automatically.
+def pipelineSettingsPathPostfix = '' as String
+// Jenkins pipeline name prefix, a string that will be cut from the beginning of yaml settings search filename.
+def pipelineNamePrefix = 'admin_' as String
+// Jenkins pipeline name postfix, a string that will be cut from the end of yaml settings search filename.
+def pipelineNamePostfix = '' as String
+// Set your ansible installation name from jenkins settings:
+def AnsibleInstallationName = 'home_local_bin_ansible' as String
+
+
+String getPipelineYamlSettings(String settingsRepoUrl, String settingsRepoBranch, String settingsPath,
+                               String settingsFilename) {
+    CF.cloneGitToFolder(String projectGitUrl, String projectGitlabBranch, String projectLocalPath = 'settings')
+}
+
 
 node('master') {
     wrap([$class: 'TimestamperBuildWrapper']) {
         CF = new org.alx.commonFunctions() as Object
-        dir('') {
-            sh 'pwd'
-        }
+
+        String pipelineYamlSettings = readPipelineYamlSettings()
+        Map pipelineSettings = parseJson(new JsonBuilder(yaml).toPrettyString())
         CF.outMsg(1, 'test library connection')
     }
 }
