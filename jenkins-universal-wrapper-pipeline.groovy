692fd0c1f412bb041feffa497a8db88a9d3284fd
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-04-07 00:49:03 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -14,30 +14,30 @@ import org.yaml.snakeyaml.*
 // 'git@github.com:alexanderbazhenoff/ansible-wrapper-settings.git'. Will be ignored when SETTINGS_GIT_BRANCH pipeline
 // parameter present and not blank.
 final SettingsGitUrl = 'https://github.com/alexanderbazhenoff/universal-wrapper-pipeline-settings' as String
-def DefaultSettingsGitBranch = 'main' as String
+final DefaultSettingsGitBranch = 'main' as String
 
 // Prefix for pipeline settings relative path inside the 'universal-wrapper-pipeline-settings' project, that will be
 // added automatically on yaml load.
-def SettingsRelativePathPrefix = 'settings' as String
+final SettingsRelativePathPrefix = 'settings' as String
 
 // Jenkins pipeline name regex, a string that will be cut from pipeline name to become a filename of yaml pipeline
 // settings to be loaded. Example: Your jenkins pipeline name is 'prefix_pipeline-name_postfix'. To load pipeline
 // settings 'pipeline-name.yml' you can use regex list: ['^prefix_','_postfix$']. FYI: All pipeline name prefixes are
 // useful to split your jenkins between your company departments (e.g: 'admin', 'devops, 'qa', 'develop', etc...), while
 // postfixes are useful to mark pipeline as a changed version of original.
-def PipelineNameRegexReplace = ['^(admin|devops|qa)_'] as ArrayList
+final PipelineNameRegexReplace = ['^(admin|devops|qa)_'] as ArrayList
 
 // Set your ansible installation name from jenkins settings.
-def AnsibleInstallationName = 'home_local_bin_ansible' as String
+final AnsibleInstallationName = 'home_local_bin_ansible' as String
 
 // Jenkins node pipeline parameter name that specifies a name of jenkins node to execute on.
-def JenkinsNodeNamePipelineParameter = 'NODE_NAME' as String
+final JenkinsNodeNamePipelineParameter = 'NODE_NAME' as String
 
 // Jenkins node tag pipeline parameter name that specifies a tag of jenkins node to execute on.
-def JenkinsNodeTagPipelineParameterName = 'NODE_TAG' as String
+final JenkinsNodeTagPipelineParameterName = 'NODE_TAG' as String
 
 // Built-in pipeline parameters, which are mandatory and not present in 'universal-wrapper-pipeline-settings'.
-def BuiltinPipelineParameters = [
+final BuiltinPipelineParameters = [
         [name       : 'SETTINGS_GIT_BRANCH',
          type       : 'string',
          regex      : '(\\*)? +(.*?) +(.*?)? ((\\[(.*?)(: (.*?) (\\d+))?\\])? ?(.*$))?',
