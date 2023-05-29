71143e3afa4448348f585221262330a2ed435f74
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-05-29 21:15:03 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -957,7 +957,7 @@ ArrayList checkOrExecutePipelineActionItem(String stageName, Map actionItem, Map
         configStructureErrorMsgWrapper(anyJenkinsNode, true, 0, String.format("'node' key in '%s' action is null. %s",
                 "This stage will run on any free Jenkins node.", printableStageAndAction))
         nodeItem.node = [:]
-        nodeItem.node.name = actionItem.node?.get('name')
+        nodeItem.node.name = actionItem.get('node')?.get('name')
         println 'yeaha2'
     } else if (actionItem.get('node') instanceof Map) {
         println 'yeahb'
