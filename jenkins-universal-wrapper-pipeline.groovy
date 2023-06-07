1ffd1376cac8b38c3eb66178429be85d0c1ba2c7
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-06-07 20:16:36 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -89,8 +89,9 @@ String applyReplaceRegexItems(String text, ArrayList regexItemsList, ArrayList r
     regexItemsList.eachWithIndex { value, Integer index ->
         text = text.replaceAll(value as CharSequence, replaceItemsList.contains(index) ? replaceItemsList[index] as
                 String : '')
-        println String.format("performing replacement of '%s' regex '%s' with '%s' element in '%s'", text, value,
-                index, replaceItemsList)
+        //println String.format("performing replacement of '%s' regex '%s' with '%s' element in '%s'", text, value,
+        //        index.toString(), replaceItemsList.toString())
+        println 'ky'
     }
     return text
 }
