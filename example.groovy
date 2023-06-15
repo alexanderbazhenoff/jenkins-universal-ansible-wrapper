f40980aba41d388d3b5912ff3d750e5667186962
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 20:15:55 +0300

    wip

--- example.groovy
+++ example.groovy
@@ -4,11 +4,13 @@
 
 node('master') {
     CF = new org.alx.troubleShooting() as Object
+    GV = new org.alx.OrgAlxGlob() as Object
     wrap([$class: 'TimestamperBuildWrapper']) {
 
         println String.format('fn from src: %s', CF.TestFunctionInSrc())
-        def gitCred = library('jenkins-shared-library-alx@devel').org.alx.OrgAlxGlob.GitCredentialsID
-        println gitCred
+        //def gitCred = library('jenkins-shared-library-alx@devel').org.alx.OrgAlxGlob.GitCredentialsID
+        //println gitCred
+        println GV.GitCredentialsID
 
 
     }
