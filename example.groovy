6c4d88650655bb015330c7833b525e67aa56c72f
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 20:04:11 +0300

    wip

--- example.groovy
+++ example.groovy
@@ -9,6 +9,7 @@ node('master') {
     wrap([$class: 'TimestamperBuildWrapper']) {
 
         println String.format('fn from src: %s', CF.TestFunctionInSrc())
+        println String.format('fn from src: %s', CF.TestFunctionInSrc2())
         println String.format('fn from class from src: %s', t.OrgAlxGlob.GitCredentialsID)
 
     }
