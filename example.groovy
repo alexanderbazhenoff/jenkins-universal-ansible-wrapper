80c1969ee0b2f53a5fa2cbdbcf8325e4456b1dd5
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 18:58:58 +0300

    wip

---
+++ example.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+@Library('jenkins-shared-library-alx@devel') _
+
+node('master') {
+    CF = new org.alx.troubleShooting() as Object
+    wrap([$class: 'TimestamperBuildWrapper']) {
+
+        println String.format('fn from src: %s', CF.TestFunctionInSrc())
+        println String.format('class from src: %s', CF.OrgAlxGlob.GitCredentialsID)
+
+    }
+}
