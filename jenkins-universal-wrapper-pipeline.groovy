d7c0152be7ad96f1ab6040ac6c45d6dae8f29e03
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-06-16 18:27:59 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -197,7 +197,7 @@ static Map findMapItemsFromList(Map map, ArrayList listOfKeysToCollect) {
  * @return - password with replaced symbols.
  */
 static String hidePasswordString(String passwordString, String replaceSymbol = '*') {
-    return replaceSymbol * passwordString.length()
+    return replaceSymbol * passwordString?.length()
 }
 
 /**
