749e3ddc9a08bba8aa60a2637ec9e7540455b8ed
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-06-21 02:26:50 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -1510,6 +1510,7 @@ ArrayList actionAnsiblePlaybookOrScriptRun(String actionLink, Map pipelineSettin
                     ansibleInstallationName)
         }
     }
+    // TODO: java.lang.IllegalArgumentException: multiply() should be called with a number of 0 or greater not: -457
     (actionOk, actionMsg) = actionClosureWrapperWithTryCatch(check, envVariables, actionClosure, actionLink,
             actionName, checkOrExecuteData, stringKeys, actionOk)
     return [actionOk, actionMsg]
