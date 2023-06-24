0e04da461f0f4368578ddf992d077dcb069b61c9
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-06-24 03:53:20 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -1546,7 +1546,7 @@ ArrayList actionAnsiblePlaybookOrScriptRun(String actionLink, Map pipelineSettin
                 'return universalPipelineWrapperBuiltIns')
         // TODO: remove from doc 'Для внутренних нужд путем запуска скриптов "как часть pipeline'а"'
         actionClosure = (checkOrExecuteData?.get(booleanSubKeys[0]) && asPartOfPipelineContentDefined) ? {
-            Map universalPipelineWrapperBuiltInsUpdate = pipelineCodeText.call()
+            Map universalPipelineWrapperBuiltInsUpdate = evaluate(pipelineCodeText)
             return [actionOk, universalPipelineWrapperBuiltIns + universalPipelineWrapperBuiltInsUpdate]
         } : (!checkOrExecuteData?.get(booleanSubKeys[0]) && scriptContentDefined) ? {
             sh scriptText
