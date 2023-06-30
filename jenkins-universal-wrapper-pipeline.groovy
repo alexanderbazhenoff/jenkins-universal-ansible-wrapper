385285436641ed473a977c09f2032ffa032e5d59
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-07-01 02:32:55 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -1719,7 +1719,7 @@ ArrayList actionDownstreamJobRun(String actionLink, Map actionLinkItem, Object e
     String copyArtifactsErrMsg = String.format("Unable to copy artifacts from %s in '%s'", actionName, actionLink)
     String copyArtifactsErrReason = dryRunMode ? " 'DRY_RUN' mode enabled." : ''
     copyArtifactsErrReason += waitForPipelineComplete ? '' : ' defined not to wait for completion.'
-    copyArtifactsErrReason += !check && copyArtifactsBuildSelector.trim() && downstreamJobNameDefined ? '' :
+    copyArtifactsErrReason += !check && !copyArtifactsBuildSelector.trim() && downstreamJobNameDefined ? '' :
             String.format(" Unable to get build number of %s: it's undefined. %s", actionName,
                     "Perhaps this job is still running or wasn't started.")
     if (!check && waitForPipelineComplete && copyArtifactsFilter.trim() && !copyArtifactsErrReason.trim()) {
