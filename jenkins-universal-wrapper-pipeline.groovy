c71cf0e4cb7fe6f8a80ae036197760274069e189
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-07-03 02:34:59 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -775,7 +775,7 @@ ArrayList mapToFormattedStringTable(Map sourceMap, Boolean createTable = false, 
             String tableEntry = (replaceKeyName?.trim() && k == replaceKeyName) ?
                     applyReplaceRegexItems(v.toString(), regexItemsList, replaceItemsList) : v.toString()
             if (createTable) {
-                println 'lolo3a tableColumnSizes[k as String]: ' + tableColumnSizes[k as String]
+                println 'lolo3a k:' + k + ' tableColumnSizes[k]: ' + tableColumnSizes[k as String]
                 Integer padSize = tableColumnSizes[k as String] - tableEntry.length()
                 println 'lolo3b padSize: ' + padSize
                 formattedStringTable += String.format('%s%s', tableEntry, ' ' * padSize)
