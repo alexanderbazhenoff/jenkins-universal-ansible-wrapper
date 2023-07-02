4a82346ff728d206372686f2d5a2e0ff9bf10c1d
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-07-02 21:01:30 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -1108,8 +1108,8 @@ ArrayList checkOrExecutePipelineActionItem(Map universalPipelineWrapperBuiltIns,
             String.format("'%s' will be skipped by conditions met: %s", printableStageAndAction, actionSkipMsgReason))
     if (actionIsCorrect && (check || allActionConditionsMet)) {
         actionMessageOutputWrapper(check, actionItem, 'before', envVariables)
-        currentBuild.displayName = !check && actionItem.get('build_name') ? actionItem.get('build_name') :
-                currentBuild.displayName
+        /*currentBuild.displayName = !check && actionItem.get('build_name') ? actionItem.get('build_name') :
+                currentBuild.displayName*/
 
         // Directory change wrapper.
         String actionItemCurrentDirectory = !check && actionItem?.get('dir') ? actionItem.get('dir').toString() : ''
