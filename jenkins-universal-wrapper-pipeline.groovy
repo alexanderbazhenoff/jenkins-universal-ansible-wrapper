c26d57d27895b213ab77010b3bdcf608e7a3bbcd
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-07-04 03:38:11 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -522,6 +522,7 @@ ArrayList templatingMapKeysFromVariables(Map assignMap, ArrayList assignmentKeys
         if (assignMap.containsKey(currentKey) && assignMap[currentKey] instanceof String) {
             def (__, Boolean assignOk, String assigned) = getTemplatingFromVariables(assignMap[currentKey].toString(),
                     envVariables, additionalVariablesBinding)
+            println 'assignMap2: ' + assignMap
             allAssignmentsPass = errorMsgWrapper(!assignOk, allAssignmentsPass, 3,
                     String.format("%s '%s' with value '%s' wasn't set properly due to undefined variable(s).",
                             keysDescription, currentKey, assignMap[currentKey].toString()))
