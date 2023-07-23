05240a3d4794a30908c78f75e21c942a15e79305
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-07-23 18:19:31 +0300

    wip

--- jenkins-universal-wrapper-pipeline.groovy
+++ jenkins-universal-wrapper-pipeline.groovy
@@ -1143,8 +1143,6 @@ ArrayList checkOrExecutePipelineActionItem(Map universalPipelineWrapperBuiltIns,
 
     // Processing action link state, updating results of current build and actions report, stop on fail handle.
     Boolean actionStructureAndLinkOk = actionStructureOk && actionLinkOk
-    if (!check && !actionStructureAndLinkOk) currentBuild.result = 'FAILURE'
-    println 'currentBuild.result: ' + currentBuild.result
     universalPipelineWrapperBuiltIns.currentBuild_result = currentBuild.result?.trim() ?: 'SUCCESS'
     println 'currentBuild_result: ' + universalPipelineWrapperBuiltIns.currentBuild_result
     Map multilineReportMap = universalPipelineWrapperBuiltIns?.get('multilineReportMap') ?
@@ -1346,6 +1344,8 @@ ArrayList checkOrExecutePipelineActionLink(String actionLink, Map nodeItem, Map 
         }
     }
     actionDetails = String.format('%s: %s', actionLink, (keysFound) ? actionDetails : '<undefined or incorrect key(s)>')
+    if (!check && !actionOk) currentBuild.result = 'FAILURE'
+    println 'currentBuild.result: ' + currentBuild.result
     return [actionOk, actionDetails, universalPipelineWrapperBuiltIns, envVariables]
 }
 
